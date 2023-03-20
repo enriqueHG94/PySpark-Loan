17940233966cc448c6a18efbbd0744f37acf33a7
Enrique Hervás Guerrero <enrique_hervas_94@hotmail.com>
2023-03-20 15:55:18 +0100

    adding schema to main code

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from pyspark.sql import SparkSession
+from schema.loan_schema import LoanEntity
 
 spark = SparkSession.builder.appName('reading CSV').getOrCreate()
 
 df = spark.read.format('csv')\
     .option('header', 'true')\
+    .schema(LoanEntity.schema)\
     .load("resources\loan_data.csv")
 
 df.show()
