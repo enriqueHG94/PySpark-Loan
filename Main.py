23fe16196bc1022eff2014d80559635b6c33dd04
Enrique Hervás Guerrero <enrique_hervas_94@hotmail.com>
2023-03-20 09:24:54 +0100

    removing unnecessary comments

--- Main.py
+++ Main.py
@@ -1,12 +1,9 @@
 from pyspark.sql import SparkSession
 
-# Crea una sesión de Spark
 spark = SparkSession.builder.appName('reading CSV').getOrCreate()
 
-# Lee el archivo CSV desde la carpeta
 df = spark.read.format('csv')\
     .option('header', 'true')\
     .load("resources\loan_data.csv")
 
-# Muestra el dataframe
 df.show()
