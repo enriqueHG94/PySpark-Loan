8ecc06349f751a576422835b3d1d3e01461b97f7
Enrique Hervás Guerrero <enrique_hervas_94@hotmail.com>
2023-03-21 12:26:38 +0100

    adding fields to class variable and restructuring schema

--- schema/loan_schema.py
+++ schema/loan_schema.py
@@ -1,29 +1,51 @@
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, LongType
 
 class LoanSchema:
+    CHECKING_STATUS_FIELD = "checking_status"
+    DURATION_FIELD = "duration"
+    CREDIT_HISTORY_FIELD = "credit_history"
+    PURPOSE_FIELD = "purpose"
+    CREDIT_AMOUNT_FIELD = "credit_amount"
+    SAVINGS_STATUS_FIELD = "savings_status"
+    EMPLOYMENT_FIELD = "employment"
+    INSTALLMENT_COMMITMENT_FIELD = "installment_commitment"
+    PERSONAL_STATUS_FIELD = "personal_status"
+    OTHER_PARTIES_FIELD = "other_parties"
+    RESIDENCE_SINCE_FIELD = "residence_since"
+    PROPERTY_MAGNITUDE_FIELD = "property_magnitude"
+    AGE_FIELD = "age"
+    OTHER_PAYMENT_PLANS_FIELD = "other_payment_plans"
+    HOUSING_FIELD = "housing"
+    EXISTING_CREDITS_FIELD = "existing_credits"
+    JOB_FIELD = "job"
+    NUM_DEPENDENTS_FIELD = "num_dependents"
+    OWN_TELEPHONE_FIELD = "own_telephone"
+    FOREIGN_WORKER_FIELD = "foreign_worker"
+    CLASS_FIELD = "class"
+
     def __init__(self):
         self.schema = StructType([
-            StructField("checking_status", StringType(), True),
-            StructField("duration", IntegerType(), True),
-            StructField("credit_history", StringType(), True),
-            StructField("purpose", StringType(), True),
-            StructField("credit_amount", LongType(), True),
-            StructField("savings_status", StringType(), True),
-            StructField("employment", StringType(), True),
-            StructField("installment_commitment", IntegerType(), True),
-            StructField("personal_status", StringType(), True),
-            StructField("other_parties", StringType(), True),
-            StructField("residence_since", IntegerType(), True),
-            StructField("property_magnitude", StringType(), True),
-            StructField("age", IntegerType(), True),
-            StructField("other_payment_plans", StringType(), True),
-            StructField("housing", StringType(), True),
-            StructField("existing_credits", IntegerType(), True),
-            StructField("job", StringType(), True),
-            StructField("num_dependents", IntegerType(), True),
-            StructField("own_telephone", StringType(), True),
-            StructField("foreign_worker", StringType(), True),
-            StructField("class", StringType(), True)
-        ])
+            StructField(self.CHECKING_STATUS_FIELD, StringType(), True),
+            StructField(self.DURATION_FIELD, IntegerType(), True),
+            StructField(self.CREDIT_HISTORY_FIELD, StringType(), True),
+            StructField(self.PURPOSE_FIELD, StringType(), True),
+            StructField(self.CREDIT_AMOUNT_FIELD, LongType(), True),
+            StructField(self.SAVINGS_STATUS_FIELD, StringType(), True),
+            StructField(self.EMPLOYMENT_FIELD, StringType(), True),
+            StructField(self.INSTALLMENT_COMMITMENT_FIELD, IntegerType(), True),
+            StructField(self.PERSONAL_STATUS_FIELD, StringType(), True),
+            StructField(self.OTHER_PARTIES_FIELD, StringType(), True),
+            StructField(self.RESIDENCE_SINCE_FIELD, IntegerType(), True),
+            StructField(self.PROPERTY_MAGNITUDE_FIELD, StringType(), True),
+            StructField(self.AGE_FIELD, IntegerType(), True),
+            StructField(self.OTHER_PAYMENT_PLANS_FIELD, StringType(), True),
+            StructField(self.HOUSING_FIELD, StringType(), True),
+            StructField(self.EXISTING_CREDITS_FIELD, IntegerType(), True),
+            StructField(self.JOB_FIELD, StringType(), True),
+            StructField(self.NUM_DEPENDENTS_FIELD, IntegerType(), True),
+            StructField(self.OWN_TELEPHONE_FIELD, StringType(), True),
+            StructField(self.FOREIGN_WORKER_FIELD, StringType(), True),
+            StructField(self.CLASS_FIELD, StringType(), True)
+            ])
 
 loan_entity = LoanSchema()
