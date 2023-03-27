3d117a5ddcd2e79fe1b177aa8b534ed87f3eefd5
Enrique Hervás Guerrero <enrique_hervas_94@hotmail.com>
2023-03-27 12:04:14 +0200

    changing the name of the app

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from pyspark.sql import SparkSession
-from schema.loan_schema import LoanEntity
+from schema.loan_schema import loan_entity
 
-spark = SparkSession.builder.appName('reading CSV').getOrCreate()
+spark = SparkSession.builder.appName('working CSV').getOrCreate()
 
 df = spark.read.format('csv')\
     .option('header', 'true')\
-    .schema(LoanEntity.schema)\
+    .schema(loan_entity.schema)\
     .load("resources\loan_data.csv")
+
 
 df.show()
