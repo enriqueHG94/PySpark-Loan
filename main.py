6a198e04de315d2b052de3ec18320a07bec7283a
Enrique Hervás Guerrero <enrique_hervas_94@hotmail.com>
2023-03-27 12:06:50 +0200

    adding transformation to the main code and displaying it on the screen

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from pyspark.sql import SparkSession
 from schema.loan_schema import loan_entity
+from services.transformations import transform_dataframe
 
 spark = SparkSession.builder.appName('working CSV').getOrCreate()
 
-df = spark.read.format('csv')\
-    .option('header', 'true')\
-    .schema(loan_entity.schema)\
+df = spark.read.format('csv') \
+    .option('header', 'true') \
+    .schema(loan_entity.schema) \
     .load("resources\loan_data.csv")
 
-
-df.show()
+transformed_df = transform_dataframe(df)
+transformed_df.show()
